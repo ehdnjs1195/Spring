12d7109c0de0affa65b380ed33b6e60fb0b600fc
ADW <ehdnjs1195@naver.com>
2020-01-29 15:56:16 +0900

    [Acorn] Spring03 프로젝트에 로그인 예제3개, 로그아웃기능 구현함.

--- Spring03_Basic/src/main/java/com/gura/spring03/users/controller/UsersController.java
+++ Spring03_Basic/src/main/java/com/gura/spring03/users/controller/UsersController.java
@@ -1,7 +1,15 @@
 package com.gura.spring03.users.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gura.spring03.users.dto.UsersDto;
 
 @Controller
 public class UsersController {
@@ -14,4 +22,82 @@ public class UsersController {
 		//view page 의 정보만 단순히 리턴해 주는 경우도 있다.
 		return "users/loginform";
 	}
+	
+	@RequestMapping("/users/login")
+	public String login(HttpServletRequest request, HttpSession session) {
+		//폼 전송되는 파라미터를 추출한다.
+		String id=request.getParameter("id");
+		String pwd=request.getParameter("pwd");
+		//유효한 정보인지 여부
+		boolean isSuccess=false;
+		if(id.equals("gura") && pwd.equals("1234")) {
+			isSuccess=true;
+			//로그인 처리를 한다.
+			session.setAttribute("id", id);
+		}
+		//로그인 성공 여부를 request 에 담는다.
+		request.setAttribute("isSuccess", isSuccess);
+		//view page 의 정보를 리턴한다.
+		return "/users/login";
+	}
+	
+	/*
+	 * 	@RequestParam 어노테이션은 전송되는 파라미터를 자동으로 추출할 때 사용한다.
+	 * 	단, 지역변수의 이름은 파라미터의 이름과 같아야 한다.
+	 */
+	@RequestMapping("/users/login2")
+	public ModelAndView login2(@RequestParam String id, @RequestParam String pwd, 	// =>getParameter를 할 필요가 없다.
+			HttpSession session, ModelAndView mView) {
+		
+		//유효한 정보인지 여부
+		boolean isSuccess=false;
+		if(id.equals("gura") && pwd.equals("1234")) {
+			isSuccess=true;
+			//로그인 처리를 한다.
+			session.setAttribute("id", id);
+		}
+		//view 페이지에서 필요한 모델을 담고
+		mView.addObject("isSuccess", isSuccess);
+		//view page 의 정보도 담고
+		mView.setViewName("/users/login");
+		return mView;
+	}
+	/*
+	 * 	@ModelAttribute 어노테이션과 함께 Dto 를 메서드의 인자로 선언하면
+	 * 	전송되는 파라미터가 자동 추출되어서 Dto 객체에 담겨서 인자로 전달된다.
+	 * 	단, 전송되는 파라미터명과 Dto 의 필드명이 같아야 한다.	(form의 파라미터명과 dto의 필드명이 같아야 한다. id와 id, pwd와 pwd)
+	 */
+	@RequestMapping("/users/login3")
+	public ModelAndView login3(@ModelAttribute UsersDto dto, HttpSession session, ModelAndView mView) {	//dto로 한 번에 추출하기.
+		//유효한 정보인지 여부
+		boolean isSuccess=false;
+		if(dto.getId().equals("gura") && dto.getPwd().equals("1234")) {
+			isSuccess=true;
+			//로그인 처리를 한다.
+			session.setAttribute("id", dto.getId());
+		}
+		//view 페이지에서 필요한 모델을 담고
+		mView.addObject("isSuccess", isSuccess);
+		//view page 의 정보도 담고
+		mView.setViewName("/users/login");
+		return mView;
+	}
+	
+	//로그아웃 요청 처리하는 메소드
+	@RequestMapping("/users/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		/*
+		 * 	forward 이동이 아닌 리다일렉트 이동 응답을 하려면
+		 *  view page 의 정보를 
+		 *  
+		 *  "redirect: 리다일렉트 시킬 절대 경로"
+		 *  
+		 *  형식으로 작성하면 된다.
+		 *  단, context path 는 작성하지 않는다.
+		 */
+		return "redirect:/home.do";
+	}
+	
+
 }
---
+++ Spring03_Basic/src/main/java/com/gura/spring03/users/dto/UsersDto.java
@@ -0,0 +1,27 @@
+package com.gura.spring03.users.dto;
+
+public class UsersDto {
+	private String id;
+	private String pwd;
+	public UsersDto(String id, String pwd) {
+		super();
+		this.id = id;
+		this.pwd = pwd;
+	}
+	//디폴트 생성자
+	public UsersDto() {}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+}
