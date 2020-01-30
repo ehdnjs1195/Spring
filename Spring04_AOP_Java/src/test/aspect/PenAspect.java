6fce4250fc4598f6e9a3289e48503b7bf574feea
ADW <ehdnjs1195@naver.com>
2020-01-30 10:08:01 +0900

    [Acorn 20.01.30] Spring04 프로젝트에 @after Aop 적용 예제 추가함

--- Spring04_AOP_Java/src/test/aspect/PenAspect.java
+++ Spring04_AOP_Java/src/test/aspect/PenAspect.java
@@ -1,5 +1,6 @@
 package test.aspect;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -9,8 +10,8 @@ import org.springframework.stereotype.Component;
 public class PenAspect {
 	/*
 	 * 	spring 이 관리하는 객체의 메서드 중에서 
-	 * 	리턴type 은 void, 메서드명은 write1
-	 * 	메서드에 전달되는 인자가 없는 메서드의
+	 * 	(1)리턴type 은 void, (2)메서드명은 write1
+	 * 	메서드에 전달되는 인자가 없는 메서드의((3)인자의 유무)
 	 *  수행 이전에 할 작업.
 	 *  
 	 *  aspectj expression(Before에 들어가는 문법! 공부는 1시간이면 충분!)
@@ -18,6 +19,10 @@ public class PenAspect {
 	@Before("execution(void write*())")	//스프링이 관리하고 있는 객체 메서드들 중에서 "execution(xxx)"모양의 갖고 있는 메서드가 수행되기 이전에! 이것을 적용해라. 이런 뜻!
 	public void prepare() {
 		System.out.println("Pen 을 준비해요!");
-	}	
+	}	//before는 해당 메서드가 호출될 때 바로 직전에 적용. after는 해당 메서드가 return 되고난 바로 직후에 적용.
 	
+	@After("execution(void write*())")
+	public void end() {
+		System.out.println("다쓰고 버려요.");
+	}
 }
--- Spring04_AOP_Java/src/test/main/MainClass.java
+++ Spring04_AOP_Java/src/test/main/MainClass.java
@@ -8,7 +8,7 @@ import test.mypac.WritingUtil;
 public class MainClass {
 	public static void main(String[] args) {
 		// init.xml 문서를 로딩해서 bean 으로 만들것들은 만들어서 관리한다.
-		ApplicationContext context=new ClassPathXmlApplicationContext("test/main/init.xml");
+		ApplicationContext context=new ClassPathXmlApplicationContext("test/main/init.xml");	//어디에 있는 xml 문서를 해석할지.
 		// 관리하고 있는 객체중에서 WritingUtil type 의 참조값 얻어오기
 		WritingUtil util=context.getBean(WritingUtil.class);
 		
