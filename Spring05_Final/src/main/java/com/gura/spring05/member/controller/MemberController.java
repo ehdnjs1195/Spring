b886e392cf439f6c78c7ed480e787f425be7d312
ADW <ehdnjs1195@naver.com>
2020-01-31 12:20:43 +0900

    [Acorn] @ModelAttribute("key") 사용법 추가.

--- Spring05_Final/src/main/java/com/gura/spring05/member/controller/MemberController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/member/controller/MemberController.java
@@ -50,9 +50,14 @@ public class MemberController {
 	 * 	단, 파라미터명과 Dto 의 필드명이 일치해야 한다.
 	 */
 	@RequestMapping("/member/insert")
-	public ModelAndView insert(@ModelAttribute MemberDto dto, ModelAndView mView) {
+	public ModelAndView insert(@ModelAttribute("dto") MemberDto dto, ModelAndView mView) {
 		dao.insert(dto);
-		mView.addObject("dto", dto);
+		/*
+		 * 	@ModelAttribute("dto") MemberDto dto 의 의미는
+		 * 	1. 전송되는 파라미터를 자동으로 추출해서 MemberDto 에 담아주기도 하고
+		 * 	2. "dto" 라는 키값으로 MemberDto 객체를 request 영역에 담아주는 역할도 한다. =>view page에서 사용.
+		 */
+//		mView.addObject("dto", dto);
 		mView.setViewName("member/insert");
 		return mView;
 	}
