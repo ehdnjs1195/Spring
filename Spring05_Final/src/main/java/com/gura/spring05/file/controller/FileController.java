9909c4c0043f451c80ea2bf61588dc0b52bea9c9
ADW <ehdnjs1195@naver.com>
2020-02-05 16:06:35 +0900

    [Acorn] /file/download.do 파일 다운로드를 view 에서 다운로드 하도록 수정함

--- Spring05_Final/src/main/java/com/gura/spring05/file/controller/FileController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/file/controller/FileController.java
@@ -44,7 +44,12 @@ public class FileController {
 	public ModelAndView download(ModelAndView mView, @RequestParam int num) {	//파일 정보를 int num을 이용해서 select 하고 
 		service.getFileData(mView, num);
 		service.addDownCount(num);
-		mView.setViewName("file/download");
+		mView.setViewName("fileDownView");	//다운로드를 view에서 할 수 있도록 해준다. 
+		/*
+		 *  /view/fileDownView.jsp 라는 의미가 되는데 jsp를 찾아가기 전에 
+		 *  찾아기 전에 fileDownView bean 을 먼저 찾는다. 찾아서 있으면 fileDownView 로 포워드 이동한다.
+		 *  => download.jsp로 응답을 하지 않겠다. 파일의 목적에 맞지 않기 때문에!!!
+		 */
 		return mView;
 	}
 }
